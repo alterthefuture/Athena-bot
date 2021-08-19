3fc05e9e20748be945946e31563b05fcac1a8680
false <80481433+ilyLuxa@users.noreply.github.com>
2021-08-18 20:02:54 -0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -21,7 +21,6 @@ class help(commands.Cog):
         if category == None:
             embed=discord.Embed(title="Athena Help Menu",description=f"Below are all the commands type `{get_prefix}help [catagory]` to get started.\n\n [Server](https://discord.com/invite/nSF4vQaW35) • [Invite](https://discord.com/api/oauth2/authorize?client_id=862536816531341312&permissions=8&scope=bot) • [Upvote](https://discordbotlist.com/bots/athena-1350/upvote)",timestamp=ctx.message.created_at,colour=discord.Colour.from_rgb(255,192,203))
             embed.add_field(name="Information",value="`Displays Information Commands.`",inline=False)
-            embed.add_field(name="Moderation",value="`Displays Moderation Commands.`",inline=False)
             embed.add_field(name="Server",value="`Displays Server Commands.`",inline=False)
             embed.add_field(name="Music",value="`Displays Music Commands.`",inline=False)
             embed.add_field(name="Anti",value="`Displays Anti Commands.`",inline=False)
@@ -31,27 +30,7 @@ class help(commands.Cog):
             embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
 
             await ctx.send(embed=embed)
-        elif category == "mod" or category == "Mod" or category == "Moderation" or category == "moderation":
-            cog = self.client.get_cog('moderation')
-            commands = cog.get_commands()
-            embed = discord.Embed(title="Athena Moderation Commands",colour=discord.Colour.from_rgb(255,192,203),description='Commands with () are optional and [] is required.',timestamp=ctx.message.created_at)
-            embed.set_footer(text=f"Requested by {ctx.author}",icon_url=ctx.author.avatar_url)
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/863200845688733736/863930582320939029/original.jpg")
-            for command in commands:
-                if command.aliases != []:
-                    alias_list = command.aliases
-                    temp_alias_list = []
-                    for i in range(len(alias_list)):
-                        temp_alias_list.append(f'.'+alias_list[i])
-                    embed.add_field(
-                        name=command.qualified_name,
-                        value=f'Description: {command.description}\nUsage: {command.usage}\nAliases: `{", ".join(temp_alias_list)}`',
-                        inline=False
-                    )
-                else:
-                    embed.add_field(name=command.qualified_name,value=f'Description: {command.description}\nUsage: {command.usage}',inline=False)
-            await ctx.send(embed=embed)
-
+      
         elif category == "music" or category == "Music":
             cog = self.client.get_cog('music')
             commands = cog.get_commands()
@@ -184,7 +163,7 @@ class help(commands.Cog):
             await ctx.send(embed=embed)
 
         elif category == "owner" or category == "Owner":
-            if ctx.author.id == 798325945463078952 or ctx.author.id == 746051373753171978:
+            if ctx.author.id == your_id_as_a_int:
                 cog = self.client.get_cog('owner')
                 commands = cog.get_commands()
                 embed = discord.Embed(title="Athena Owner Commands",colour=discord.Colour.from_rgb(255,192,203),description='Commands with () are optional and [] is required.',timestamp=ctx.message.created_at)
